66e6a5a6259f3143c5b2de503472c217f0568f91
Ruslan <ruslanlapin@yandex.ru>
2024-08-05 15:39:39 +0300

    first commit Подготовил компоненты

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,37 @@
 import React from 'react';
 import './App.css';
+import styled from "styled-components";
 
 function App() {
   return (
     <div className="App">
-
+      <StyledImg></StyledImg>
+      <Title>Headline</Title>
+      <Text>Faucibus. Faucibus. Sit sit sapien sit tempusrisu ut. Sit molestie ornare in venen.</Text>
+      <Button1>See more</Button1>
+      <Button2>Save</Button2>
     </div>
   );
 }
 
 export default App;
+
+const StyledImg = styled.img`
+
+`
+
+const Title = styled.h1`
+
+`
+
+const Text = styled.p `
+
+`
+
+const Button1 = styled.button `
+    
+`
+
+const Button2 = styled.button `
+    
+`
