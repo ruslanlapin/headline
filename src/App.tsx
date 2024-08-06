4ee52f9d7e5539470b612ef3819c9f2c6005170b
Ruslan <ruslanlapin@yandex.ru>
2024-08-06 22:45:04 +0300

    first commit Доработал структуру компонент и стилизовал кнопки

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,24 @@ function App() {
       <StyledImg></StyledImg>
       <Title>Headline</Title>
       <Text>Faucibus. Faucibus. Sit sit sapien sit tempusrisu ut. Sit molestie ornare in venen.</Text>
-      <Button1>See more</Button1>
-      <Button2>Save</Button2>
+        <BoxButton>
+            <Button1>See more</Button1>
+            <Button2>Save</Button2>
+        </BoxButton>
+
     </div>
   );
 }
 
 export default App;
+
+const Card = styled.div`
+
+`
+
+const Container = styled.div`
+
+`
 
 const StyledImg = styled.img`
 
@@ -28,10 +39,27 @@ const Text = styled.p `
 
 `
 
-const Button1 = styled.button `
-    
+const BoxButton = styled.div `
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
 `
 
-const Button2 = styled.button `
-    
+const Button1 = styled.button `
+    border: none;
+    background-color: #4E71FE;
+    width: 86px;
+    height: 30px;
+    border-radius: 5px;
+    padding: 4px 20px 6px 20px;
+    color: white;
+    font-size: 10px;
+`
+
+const Button2 = styled(Button1) `
+    border: 2px solid #4E71FE;
+    background-color: white;
+    color: #4E71FE;
 `
